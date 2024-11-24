d59b7d2ea5e70e3f3e66c0f8387dd5558dcbbb89
Owen Pratama S. <92712038+2122600018-Owen@users.noreply.github.com>
2024-11-24 19:57:54 +0700

    Update program website
    + Menambahkan fungsi agar website dan database kompatibel dengan webhosting Microsoft Azure
    + Fix untuk fungsi tampilan kondisi (state) dari Solenoid dan Pompa Air
    + Menambahkan fungsi agar dapat melakukan kontrol Solenoid dari web

--- Website AQUASMART/aqua/login.php
+++ Website AQUASMART/aqua/login.php
@@ -6,34 +6,40 @@ if (isset($_POST['login'])) {
 
     $username = $_POST['username'];
     $password = md5($_POST['password']);
-
-    $sql = "SELECT * FROM `tbl_user` WHERE `username`='$username' AND `password`='$password'";
+    $sql = "SELECT * FROM `tabl_user` WHERE `username`='$username' AND `password`='$password'";
     $result = mysqli_query($conn, $sql);
 
     if (empty($_POST['username']) && empty($_POST['password'])) {
         echo "<script>alert('Please Fill Username and Password');</script>";
         exit;
-    } elseif (empty($_POST['password'])) {
+    } 
+    elseif (empty($_POST['password'])) {
         echo "<script>alert('Please Fill Password');</script>";
         exit;
-    } elseif (empty($_POST['username'])) {
+    } 
+    elseif (empty($_POST['username'])) {
         echo "<script>alert('Please Fill Username);</script>";
         exit;
-    } else {
+    } 
+    else {
         if (mysqli_num_rows($result) > 0) {
             $row = mysqli_fetch_array($result);
             $name = $row['name'];
             $username = $row['username'];
             $password = $row['password'];
-
-
-            if ($username == $username && $password == $password) {
+            if (1) {
                 $_SESSION['name'] = $name;
                 $_SESSION['username'] = $username;
                 $_SESSION['password'] = $password;
-                header('location:welcome.php');
+                echo "<script>window.location.href='welcome.php';</script>";
+                exit;
             }
-        } else {
+            else {
+                echo "<script>alert('Step 2 Invalid Username or Password');</script>";
+                exit;
+            }
+        }
+        else {
             echo "<script>alert('Invalid Username or Password');</script>";
             exit;
         }
